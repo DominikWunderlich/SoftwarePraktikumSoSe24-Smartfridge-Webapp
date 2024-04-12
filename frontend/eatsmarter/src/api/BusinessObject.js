8d186f4df15ea5c2ad3e3cfa61637e2caf35cb63
Johnny Luu <jl150@hdm-stuttgart.de>
2024-04-12 14:39:24 +0200

    Frontend: BusinessObject & WgBO erstellt

---
+++ frontend/eatsmarter/src/api/BusinessObject.js
@@ -0,0 +1,29 @@
+/**
+ * Basisklasse für alle BusinessObjekte mit Id per default
+ */
+
+export default class BusinessObject{
+    // Null Constructor
+    constructor() {
+        this.id = 0;
+    }
+
+    // Id für das BusinessObjekt setzen
+    setID(aId){
+        this.id = aId;
+    }
+
+    // Id des BusinessObjekts ausgeben
+    getID(){
+        return this.id;
+    }
+
+    // Objekt als String ausgeben - Nützlich fürs debugging
+    toString(){
+        let result = "";
+        for (var prop in this){
+            result += prop + ": " + this[prop] + " ";
+        }
+        return result;
+    }
+}
