322f14aa9e0a7c0fc29e862237d9afad35000563
MerveBirinci <ma131@hdm-stuttgart.de>
2024-04-19 15:55:48 +0200

    paages angelegt

--- frontend/eatsmarter/src/App.js
+++ frontend/eatsmarter/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom
 import RegisterWG from "./pages/registerWG";
 import Homepage from "./pages/Homepage";
 import LoginPerson from "./pages/loginPerson";
+import Lebensmittelverwaltung from "./pages/Lebensmittel-Verwaltung";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
               <Route path="wg" element={<RegisterWG />} />
               <Route path="/home" element={<Homepage />} />
               <Route path="/login" element={<LoginPerson />} />
+              <Route path="/lebensmittelverwaltung" element={<Lebensmittelverwaltung />} />
             </Routes>
           </Router>
       </div>
