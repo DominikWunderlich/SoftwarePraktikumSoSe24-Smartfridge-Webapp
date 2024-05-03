11b04feb507feb26168888fbc3ce7bc21dec34aa
JustinUgwu <ju010@hdm-stuttgart.de>
2024-05-03 13:26:36 +0200

    Update Kuehlschrank.py

--- src/server/bo/Kuehlschrank.py
+++ src/server/bo/Kuehlschrank.py
@@ -1,24 +1,33 @@
 from server.bo.BusinessObject import BusinessObject as bo
+from server.bo.Lebensmittel import Lebensmittel
 
 
 class Kuehlschrank(bo):
     def __init__(self):
         super().__init__()
-        self.lebensmittel_liste = []
+        self.lebensmittel_dict = {}
 
-    def get_lebensmittel_liste(self):
-        return self.lebensmittel_liste
+    def get_lebensmittel(self, lebensmittel_id):
+        """Gibt ein Lebensmittel anhand seiner ID aus dem Kühlschrank zürck"""
+        return self.lebensmittel_dict[lebensmittel_id]
     
-    def set_lebensmittel_liste(self, lebensmittel_liste):
-        self.lebensmittel_liste = lebensmittel_liste
+    def get_lebensmittelliste_by_name(self):
+        """Gibt eine Liste der Namen der Lebensmittel im Kühlschrank zurück."""
+        return [lebensmittel.get_lebensmittlename() for lebensmittel in self.lebensmittel_dict.values()]
 
     def __str__(self):
-        return "Kuehlschrank: {}".format(self.get_id())
+        """Gibt eine textuelle Darstellung des Kühlschranks zurück."""
+        lebensmittel_str = ", ".join(self.get_lebensmittelliste_by_name())
+        return f"Kuehlschrank: {self.get_id()}, Lebensmittel: {lebensmittel_str}"
 
     @staticmethod
     def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in ein Kuehlschrank-Objekt."""
         ks = Kuehlschrank()
         ks.set_id(dictionary['id'])
-        ks.set_lebensmittel_liste(dictionary.get('lebensmittel_liste', []))
+        lebensmittel_liste = dictionary.get('lebensmittel_liste', [])
+        for lebensmittel_dict in lebensmittel_liste:
+            lebensmittel = Lebensmittel.from_dict(lebensmittel_dict)
+            ks.add_lebensmittel(lebensmittel)
         return ks
 
