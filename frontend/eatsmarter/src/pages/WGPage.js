5b6130bb1e630dfb34f9b571e18e737ad32a252f
Michael Fezer <michael-fezer@t-online.de>
2024-05-03 20:16:46 +0200

    Unterschiedliche NavBar für registerWG und WG

--- frontend/eatsmarter/src/pages/WGPage.js
+++ frontend/eatsmarter/src/pages/WGPage.js
@@ -1,7 +1,76 @@
 import React, {useEffect, useState} from "react";
 import EatSmarterAPI from "../api/EatSmarterAPI";
 import '../sytles/WG-Landingpage.css';
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+
+
+function NavBar({ currentUser, onSignOut }) {
+
+    const [state, setState] = useState({
+        menuAnchor: null,
+    })
+
+    // Handle Funktionen, um zu prüfen, ob das Menü geöffnet ist oder nicht
+ 	const handleOpen = (event) =>{
+		setState({...state, menuAnchor: event.currentTarget})
+	}
+	const handleClose = () => {
+		setState({...state, menuAnchor: null})
+	}
+
+    return (
+        <AppBar>
+            <Toolbar>
+                <Button color="inherit" component={Link} to="/wg/:wgName">
+                    Home
+                </Button>
+                <Button color="inherit" component={Link} to="/RezeptAnzeigen">
+                    Recipes
+                </Button>
+                <Button color="inherit" component={Link} to="/lebensmittelverwaltung">
+                    Fridge
+                </Button>
+                <Button color="inherit" component={Link} to="/wg">
+                    WG
+                </Button>
+                {/*Wenn CurrentUser existiert, wird der Avatar und das Menü gerendert*/}
+                {currentUser && (
+                    <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
+                        <Avatar
+                            src={currentUser.photoURL}
+                            alt={currentUser.displayName}
+                            onClick={handleOpen}
+                            style={{ cursor: 'pointer' }}
+                        />
+                        <Menu
+                            anchorEl={state.menuAnchor}
+                            open={Boolean(state.menuAnchor)}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                        >
+                            <MenuItem>{currentUser.displayName}</MenuItem>
+                            <MenuItem onClick={onSignOut}>Abmelden</MenuItem>
+                        </Menu>
+                    </div>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+}
 
 
 function WGPage(props) {
@@ -111,7 +180,7 @@ function WGPage(props) {
     return (
         <div>
             {/*TODO: Mit Navbar ersetzen*/}
-            <p>Platzhalter für NavBar</p>
+            <NavBar currentUser={props.user} />
             <div className='container'>
                 {/*Abfrage, ob wg nicht null*/}
                 {wg && (
