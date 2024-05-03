006869320961d06eb334057b00cb51470edec1a6
JustinUgwu <ju010@hdm-stuttgart.de>
2024-05-03 09:43:01 +0200

    Update Kuehlschrank.py

--- src/server/bo/Kuehlschrank.py
+++ src/server/bo/Kuehlschrank.py
@@ -1,9 +1,16 @@
-from BusinessObject import BusinessObject as bo
+from server.bo.BusinessObject import BusinessObject as bo
 
 
 class Kuehlschrank(bo):
     def __init__(self):
         super().__init__()
+        self.lebensmittel_liste = []
+
+    def get_lebensmittel_liste(self):
+        return self.lebensmittel_liste
+    
+    def set_lebensmittel_liste(self, lebensmittel_liste):
+        self.lebensmittel_liste = lebensmittel_liste
 
     def __str__(self):
         return "Kuehlschrank: {}".format(self.get_id())
@@ -12,3 +19,6 @@ class Kuehlschrank(bo):
     def from_dict(dictionary=dict()):
         ks = Kuehlschrank()
         ks.set_id(dictionary['id'])
+        ks.set_lebensmittel_liste(dictionary.get('lebensmittel_liste', []))
+        return ks
+
