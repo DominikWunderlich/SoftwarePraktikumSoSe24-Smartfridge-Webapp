1cb1b67d79e0a678b86a7e8677047585af23365d
Dominik Wunderlich <dw082@hdm-stuttgart.de>
2024-06-21 15:35:44 +0200

    Fix redirecting if a person is a wg.

--- frontend/eatsmarter/src/pages/registerWG.js
+++ frontend/eatsmarter/src/pages/registerWG.js
@@ -46,8 +46,7 @@ function WGLandingpage(props) {
         EatSmarterAPI.getAPI().getUserByGID(props.user.uid)
             .then((UserInWg) => {
                 // Redirect user based on wether the user is in a wg or not.
-                if (UserInWg.length > 0) {
-                    // TODO: Pfad von der Homepage umbenennen
+                if (UserInWg[0].wgId != null) {
                     navigate("/wg/:wgName");
                 } else {
                     navigate("/registerWg")
@@ -59,7 +58,6 @@ function WGLandingpage(props) {
     
     return (
         <div>
-            {/*TODO: neue Navbar einfügen, in welcher nur die registerWg Seite anklickbar ist*/}
             <NavBarRegisterWg currentUser={props.user} onSignOut={props.onSignOut}/> <br/> <br/>
             <div className='container'>
                 <div className="inner-container">
